f87fd6458add0223ac21dbf47309572a922f004e
YounesAO <younes.aitouahda@gmail.com>
2023-02-21 13:29:56 +0000

    route edit

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ function App() {
 
         <Route index element={<Home />}/>
         <Route path='about' element ={<About/>}/>
+        <Route path='projects' element ={<Projects/>}/>
 
         <Route path='contact' element ={<Contact/>}/>
       
       </Route>
-      <Route path='projects' element ={<Projects/>}/>
 
     </Routes>
     </>
